e44b0bc4f568bcfa237001eadd54e3aed73796c0
DitaMatata <60081029+DitaMatata@users.noreply.github.com>
2021-06-18 23:19:36 -0500

    Update dankMemer.py

--- dankMemer.py
+++ dankMemer.py
@@ -5,6 +5,8 @@ import random
 import os
 import colored
 from colored import fg, bg, attr, stylize
+import sys
+import multiprocessing
 
 r = fg(241) # Setup color variables
 r2 = fg(255)
@@ -25,24 +27,25 @@ print(stylize("""
 
 
 PERIOD_OF_TIME = int(input(f"{r2}[{b}?{r2}] Time in seconds: "))
+
 while True:    
     token = input(f"\n{r2}[{b}?{r2}] Token: ")
     channel = input(f"{r2}[{b}?{r2}] Channel Id: ")
-
+    startime = time.time()
     def execute_command(command = "", cooldown = 0):
         print(f"{r2}[{b}!{r2} Loaded: '{command}' With cooldown of {cooldown} Seconds")
-        while True:
-            requests.post(
-                f"https://discord.com/api/channels/{channel}/messages",
-                data = {'content': command},
-                headers = {
-                    'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
-                    'Authorization' : token
-                }
-            )
-            print(f"{r2}[{b}+{r2}] '{command}' Ran successfully")
 
-            time.sleep(cooldown + random.randint(2, 10))
+        requests.post(
+            f"https://discord.com/api/channels/{channel}/messages",
+            data = {'content': command},
+            headers = {
+                'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+                'Authorization' : token
+            }
+        )
+        print(f"{r2}[{b}+{r2}] '{command}' Ran successfully")
+
+        time.sleep(cooldown + random.randint(2, 10))
 
     commands = {
         "pls beg" : 45,
@@ -53,18 +56,18 @@ while True:
     }
 
     print()
+    
 
-    startime = time.time()
 
+    
     for cmd, cooldown in commands.items():
-        threading.Thread(target = execute_command, kwargs = {"command" : cmd, "cooldown" : cooldown}).start()
+        t = threading.Thread(target = execute_command, kwargs = {"command" : cmd, "cooldown" : cooldown})
+        t.start()
         print("sleep")
         time.sleep(5)
         if time.time() >= startime + PERIOD_OF_TIME:
             print("Time limit exceeded")
-            time.sleep(3)
-            os._exit(1)
+            break
         else:
             continue
-
-        
+    break
