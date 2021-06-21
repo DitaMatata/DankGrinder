3aa5515bf3582d64dbbdf087ebce6f0dd671a3a8
DitaMatata <60081029+DitaMatata@users.noreply.github.com>
2021-06-20 21:34:35 -0500

    Update dankMemer.py

--- dankMemer.py
+++ dankMemer.py
@@ -5,8 +5,6 @@ import random
 import os
 import colored
 from colored import fg, bg, attr, stylize
-import sys
-import multiprocessing
 
 r = fg(241) # Setup color variables
 r2 = fg(255)
@@ -15,37 +13,39 @@ w = fg(15)
 red = fg(1)
 
 print(stylize("""
-
 \t\t██████╗  █████╗ ███╗   ██╗██╗  ██╗     ██████╗ ██████╗ ██╗███╗   ██╗██████╗ ███████╗██████╗ 
 \t\t██╔══██╗██╔══██╗████╗  ██║██║ ██╔╝    ██╔════╝ ██╔══██╗██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
 \t\t██║  ██║███████║██╔██╗ ██║█████╔╝     ██║  ███╗██████╔╝██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
 \t\t██║  ██║██╔══██║██║╚██╗██║██╔═██╗     ██║   ██║██╔══██╗██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
 \t\t██████╔╝██║  ██║██║ ╚████║██║  ██╗    ╚██████╔╝██║  ██║██║██║ ╚████║██████╔╝███████╗██║  ██║
-\t\t╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
-                                                                                            
+\t\t╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝                                                                                          
 """, red))
 
 
 PERIOD_OF_TIME = int(input(f"{r2}[{b}?{r2}] Time in seconds: "))
+token = input(f"{r2}[{b}?{r2}] Token: ")
+channel = input(f"{r2}[{b}?{r2}] Channel Id: ")
 
 while True:    
-    token = input(f"\n{r2}[{b}?{r2}] Token: ")
-    channel = input(f"{r2}[{b}?{r2}] Channel Id: ")
     startime = time.time()
     def execute_command(command = "", cooldown = 0):
-        print(f"{r2}[{b}!{r2} Loaded: '{command}' With cooldown of {cooldown} Seconds")
+        while True:
+            if time.time() >= startime + PERIOD_OF_TIME:
+                print("Time limit exceeded")
+                break
+            print(f"{r2}[{b}!{r2}] Loaded: '{command}' With cooldown of {cooldown} Seconds")
 
-        requests.post(
-            f"https://discord.com/api/channels/{channel}/messages",
-            data = {'content': command},
-            headers = {
-                'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
-                'Authorization' : token
-            }
-        )
-        print(f"{r2}[{b}+{r2}] '{command}' Ran successfully")
+            requests.post(
+                f"https://discord.com/api/channels/{channel}/messages",
+                data = {'content': command},
+                headers = {
+                    'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+                    'Authorization' : token
+                }
+            )
+            print(f"{r2}[{b}+{r2}] '{command}' Ran successfully")
 
-        time.sleep(cooldown + random.randint(2, 10))
+            time.sleep(cooldown + random.randint(2, 10))
 
     commands = {
         "pls beg" : 45,
@@ -54,20 +54,8 @@ while True:
         "pls dig" : 40,
         "pls daily" : 86400
     }
-
-    print()
-    
-
-
     
     for cmd, cooldown in commands.items():
-        t = threading.Thread(target = execute_command, kwargs = {"command" : cmd, "cooldown" : cooldown})
-        t.start()
-        print("sleep")
+        threading.Thread(target = execute_command, kwargs = {"command" : cmd, "cooldown" : cooldown}).start()
         time.sleep(5)
-        if time.time() >= startime + PERIOD_OF_TIME:
-            print("Time limit exceeded")
-            break
-        else:
-            continue
     break
