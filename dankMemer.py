d28df7598cd17576f4bfbe5c363c46079bea3ed5
DitaMatata <60081029+DitaMatata@users.noreply.github.com>
2021-06-21 19:15:58 -0500

    Update dankMemer.py

--- dankMemer.py
+++ dankMemer.py
@@ -2,15 +2,63 @@ import requests
 import threading
 import time
 import random
-import os
+import sys, os
 import colored
 from colored import fg, bg, attr, stylize
+import keyboard 
+import tkinter as tk
+from tkinter import ttk, messagebox 
 
 r = fg(241) # Setup color variables
 r2 = fg(255)
 b = fg(31)
 w = fg(15)
 red = fg(1)
+green = fg(2)
+root = tk.Tk()
+root.withdraw()
+__version__ = '1.3'
+_AppName_ = "DankGrinder"
+
+def download_file(url, filename=''):
+    try:
+        if filename:
+            pass            
+        else:
+            filename = req.url[downloadUrl.rfind('/')+1:]
+
+        with requests.get(url) as req:
+            with open(filename, 'wb') as f:
+                for chunk in req.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+            return filename
+    except Exception as e:
+        print(e)
+        return None
+
+try:
+    # -- Online Version File
+    # -- Replace the url for your file online with the one below.
+    response = requests.get(
+        'https://raw.githubusercontent.com/DitaMatata/DankGrinder/main/version.txt')
+    data = response.text
+
+    if float(data) > float(__version__):
+        messagebox.showinfo('Software Update', 'Update Available!')
+        mb1 = messagebox.askyesno('Update!', fr'Do you want to update to v{data}?')
+        if mb1 is True:
+            downloadLink = 'https://github.com/DitaMatata/DankGrinder/releases/download/v1.2/DankGrinder.-.v1.4.exe'
+            download_file(downloadLink, 'DankGrinder - v1.4.exe')
+            sys.exit()
+        elif mb1 == False:
+            os.system("cls")
+            pass
+    else:
+        messagebox.showinfo('Software Update', 'No Updates are Available.')
+except Exception as e:
+    messagebox.showinfo('Software Update', 'Unable to Check for Update, Error:' + str(e))
+
 
 print(stylize("""
 \t\t██████╗  █████╗ ███╗   ██╗██╗  ██╗     ██████╗ ██████╗ ██╗███╗   ██╗██████╗ ███████╗██████╗ 
